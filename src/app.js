18e4e917f2a07ddda092c91e6cf9d25a6b933d4c
Carlos Cruz <carturo99@gmail.com>
2022-10-13 23:09:45 -0500

    Created routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 
+const { router } = require("./routes/index.js");
+
 const app = express();
+
+app.use(express.json());
+app.use("/api", router);
 
 module.exports.app = app;
